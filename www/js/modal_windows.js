9ef697fd5e61c3cf291a1161ae528d91d6314e3f
hayashizaki <hayashizaki-yasunari@play-smart.info>
2015-09-08 20:49:55 +0900

    js側からもモーダルの表示をコントロール出来るよう変更

--- www/js/modal_windows.js
+++ www/js/modal_windows.js
@@ -1,11 +1,7 @@
-$(function(){
-    // スクロールバーの横幅を取得
-    $('html').append('<div class="scrollbar" style="overflow:scroll;"></div>');
-    var scrollsize = window.innerWidth - $('.scrollbar').prop('clientWidth');
-    $('.scrollbar').hide();
+var kamachare = kamachare || {};
 
-    // 「.modal-open」をクリック
-    $('.modal-open').click(function(){
+kamachare.modal = {
+    open : function(modalId){
         // html、bodyを固定（overflow:hiddenにする）
         $('html, body').addClass('lock');
 
@@ -16,9 +12,9 @@ $(function(){
         $('.modal-overlay').fadeIn('slow');
 
         // モーダルコンテンツのIDを取得
-        var modal = '#' + $(this).attr('data-target');
+        var modal = modalId ||'#' + $(this).attr('data-target');
 
-         // モーダルコンテンツを囲む要素を追加
+        // モーダルコンテンツを囲む要素を追加
         $(modal).wrap("<div class='modal-wrap'></div>");
 
         // モーダルコンテンツを囲む要素を表示
@@ -27,7 +23,7 @@ $(function(){
         // モーダルコンテンツの表示位置を設定
         modalResize();
 
-         // モーダルコンテンツフェードイン
+        // モーダルコンテンツフェードイン
         $(modal).fadeIn('slow');
 
         // モーダルコンテンツをクリックした時はフェードアウトしない
@@ -37,6 +33,7 @@ $(function(){
 
         // 「.modal-overlay」あるいは「.modal-close」をクリック
         $('.modal-wrap, .modal-close').off().click(function(){
+            var modal = modalId;
             // モーダルコンテンツとオーバーレイをフェードアウト
             $(modal).fadeOut('slow');
             $('.modal-overlay').fadeOut('slow',function(){
@@ -46,7 +43,7 @@ $(function(){
                 $('.modal-overlay').remove();
                 // モーダルコンテンツを囲む要素を削除
                 $(modal).unwrap("<div class='modal-wrap'></div>");
-           });
+            });
         });
 
         // リサイズしたら表示位置を再取得
@@ -65,20 +62,49 @@ $(function(){
             var mh = $(modal).outerHeight(true);
 
             // モーダルコンテンツの表示位置を設定
+            var x, y;
             if ((mh > h) && (mw > w)) {
                 $(modal).css({'left': 0 + 'px','top': 0 + 'px'});
             } else if ((mh > h) && (mw < w)) {
-                var x = (w - scrollsize - mw) / 2;
+                x = (w - scrollsize - mw) / 2;
                 $(modal).css({'left': x + 'px','top': 0 + 'px'});
             } else if ((mh < h) && (mw > w)) {
-                var y = (h - scrollsize - mh) / 2;
+                y = (h - scrollsize - mh) / 2;
                 $(modal).css({'left': 0 + 'px','top': y + 'px'});
             } else {
-                var x = (w - mw) / 2;
-                var y = (h - mh) / 2;
+                x = (w - mw) / 2;
+                y = (h - mh) / 2;
                 $(modal).css({'left': x + 'px','top': y + 'px'});
             }
         }
 
-    });
+    },
+    close : function(modalId,callBack){
+        if(modalId){
+            var modal = modalId;
+            // モーダルコンテンツとオーバーレイをフェードアウト
+            $(modal).fadeOut('slow');
+            $('.modal-overlay').fadeOut('slow',function(){
+                // html、bodyの固定解除
+                $('html, body').removeClass('lock');
+                // オーバーレイを削除
+                $('.modal-overlay').remove();
+                // モーダルコンテンツを囲む要素を削除
+                $(modal).unwrap("<div class='modal-wrap'></div>");
+                if(callBack){
+                    callBack();
+                }
+            });
+        }
+    }
+};
+
+$(function(){
+    // スクロールバーの横幅を取得
+    $('html').append('<div class="scrollbar" style="overflow:scroll;"></div>');
+    var scrollsize = window.innerWidth - $('.scrollbar').prop('clientWidth');
+    $('.scrollbar').hide();
+
+    // 「.modal-open」をクリック
+    $('.modal-open').click(kamachare.modal.open);
 });
