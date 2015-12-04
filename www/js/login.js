3f952c937c8b0b736ad44daae4da31daafa3a42f
masuzawa nobuyoshi <mazack928@gmail.com>
2015-12-05 07:53:25 +0900

    parseへのユーザー登録部分の追加

--- www/js/login.js
+++ www/js/login.js
@@ -1,4 +1,8 @@
 $(function(){
+
+    // parse initialize //
+    Parse.initialize("nSXGhoP36HfTirY21zKhglOB3FXPRjVu04S3alfK", "5aQYFbFo5DYoKsZgDUi9ZoXoOayi6VQuoXMQdWDS");
+
     $('#send_button').click(function(){
         var errorMessage = '';
         var angle = 0;
@@ -16,21 +20,47 @@ $(function(){
 
         if(errorMessage === ''){
 
-            kamachare.modal.open('#loading', false);
+            var user = new Parse.User();
+            user.set("username", userName);
+            user.set("password", '1234');
+            user.set("email", email);
 
-            fakeUpdate(function(){
-                kamachare.modal.close('#loading',
-                function(){
-                    kamachare.modal.open('#success', false);
-                    window.localStorage.setItem(kamachare.localStoreKey.loggedIn, true);
-                    window.localStorage.setItem(kamachare.localStoreKey.userName, userName);
-                    window.localStorage.setItem(kamachare.localStoreKey.userEmail, email);
-                    setTimeout(function(){
-                        kamachare.modal.close('#success');
-                        history.back();
-                    },2000);
-                });
+            user.signUp(null, {
+              success: function(user) {
+
+                alert("登録ありがとうございました。");
+
+              },
+              error: function(user, error) {
+                // Show the error message somewhere and let the user try again.
+                switch (error.code) {
+                case 202 :
+                    alert("入力した表示名は既に使用されています。");
+                    break;
+                case 203 :
+                    alert("入力したメールアドレスは既に使用されています。");
+                    break;
+                default:
+                    alert("Error: " + error.code + " " + error.message);
+                };
+              }
             });
+
+//            kamachare.modal.open('#loading', false);
+
+//           fakeUpdate(function(){
+//                kamachare.modal.close('#loading',
+//                function(){
+//                    kamachare.modal.open('#success', false);
+//                    window.localStorage.setItem(kamachare.localStoreKey.loggedIn, true);
+//                    window.localStorage.setItem(kamachare.localStoreKey.userName, userName);
+//                    window.localStorage.setItem(kamachare.localStoreKey.userEmail, email);
+//                    setTimeout(function(){
+//                        kamachare.modal.close('#success');
+//                        history.back();
+//                    },2000);
+//                });
+//            });
         }else{
             alert(errorMessage);
         }
